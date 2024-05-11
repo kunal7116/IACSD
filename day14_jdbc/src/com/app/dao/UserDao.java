88558067e70df2627d5a56e2ce9e32bb5ced7961
Kunal Raval <ravalkunal308@gmail.com>
2024-05-11 15:30:47 +0530

    JDBC PROJECT ADDED

---
+++ day14_jdbc/src/com/app/dao/UserDao.java
@@ -0,0 +1,21 @@
+package com.app.dao;
+
+import java.sql.Date;
+import java.sql.SQLException;
+import java.util.List;
+
+import com.app.entities.User;
+
+public interface UserDao {
+//add a method for user's signin
+	User signIn(String email, String password) throws SQLException;
+
+	// add a method for getting user details(not admin) born between dates
+	List<User> getUserDetails(Date begin, Date end) throws SQLException;
+	// add a method for voter reg.
+	String voterRegistration(User newVoter) throws SQLException;
+	
+	//add a method for changing pass
+	String changePass(String email,String oldPassword,String newPassword) throws SQLException;
+	
+}
